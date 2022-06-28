b609b778a2fb3f801f853c23392945b8b5734836
Pol Gubau <gubaupol@gmail.com>
2022-06-28 13:25:17 +0200

    ft: changed animation props

--- src/components/Card/animations.js
+++ src/components/Card/animations.js
@@ -1,2 +1,10 @@
-export const openSpring = { type: "spring", stiffness: 200, damping: 25 };
-export const closeSpring = { type: "spring", stiffness: 300, damping: 25 };
+export const openSpring = {
+  type: "spring",
+  stiffness: 400,
+  damping: 70,
+};
+export const closeSpring = {
+  type: "spring",
+  stiffness: 500,
+  damping: 70,
+};
