4f42b2ec68b815437cfe37d28620da6d10b27452
Pol Gubau <gubaupol@gmail.com>
2022-06-28 13:26:04 +0200

    now you can pass images

--- src/components/Card/Image.js
+++ src/components/Card/Image.js
@@ -2,6 +2,9 @@ import * as React from "react";
 import { motion, useInvertedScale } from "framer-motion";
 import { closeSpring } from "./animations";
 import "./Image.css";
+import { useState } from "react";
+import { GrFormPreviousLink, GrFormNextLink } from "react-icons/gr";
+
 export const Image = ({
   id,
   path,
@@ -10,18 +13,45 @@ export const Image = ({
   pointOfInterestX = 0,
   pointOfInterestY = 0,
   backgroundColor,
+  imagesNum = 4,
 }) => {
+  const [imageIndex, setImageIndex] = useState(0);
   const inverted = useInvertedScale();
-
+  // si hay imagen donde ir, la ponemos en el estado
+  const nextImage = (d) => {
+    if (imageIndex + d > -1 && imageIndex + d < imagesNum) {
+      setImageIndex(imageIndex + d);
+    }
+  };
   return (
     <motion.div
       className="card-image-container"
       style={{ ...inverted, backgroundColor, originX: 0, originY: 0 }}
     >
-      
+      {isSelected && (
+        <div className="buttons-image">
+          <motion.button
+            className="controls-image prev-image"
+            onClick={() => {
+              nextImage(-1);
+            }}
+          >
+            <GrFormPreviousLink />
+          </motion.button>
+          <motion.button
+            className="controls-image next-image"
+            onClick={() => {
+              nextImage(1);
+            }}
+          >
+            <GrFormNextLink />
+          </motion.button>
+        </div>
+      )}
+
       <motion.img
         className="card-image"
-        src={`images/${path}`}
+        src={`images/${id}/${imageIndex}.webp`}
         alt=""
         width={isSelected ? `${width + 100}px` : `${width}px`}
         initial={false}
