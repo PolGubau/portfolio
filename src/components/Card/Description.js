52153c33c2a0de42738464949ef188be2a9ed00c
Pol Gubau <gubaupol@gmail.com>
2022-06-28 13:25:51 +0200

    more details in description

--- src/components/Card/Description.js
+++ src/components/Card/Description.js
@@ -1,17 +1,43 @@
 import React from "react";
+import { Languages } from "src/Service/Consts";
 import "./Description.css";
-export default function Description({ tags, description, lang }) {
+import { BsSearch } from "react-icons/bs";
+
+export default function Description({
+  tags = "",
+  description = "",
+  lang = "English",
+  madeFor = "",
+  link = "",
+}) {
   return (
     <>
-      <section>
-        <ul className="tagsContainer">
+      <header className="Description-header">
+        <ul className="Description-tagsContainer">
           {tags.map((tag) => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
-      </section>
+        <p>
+          {lang === Languages[0] && madeFor[lang] && "For "}
+          {lang === Languages[1] && madeFor[lang] && "Para "}
+          {lang === Languages[2] && madeFor[lang] && "Per a "}
+          <b>{madeFor[lang]}</b>.
+        </p>
+      </header>
       <section>
-        <p className="descriptionText">{description[lang]}</p>
+        <p className="Description-descriptionText">{description[lang]}</p>
+      </section>
+      <section className="Description-footer">
+        <a className="Description-search" href={link} target="_blank">
+          <p>
+            {lang === Languages[0] && "Search "}
+            {lang === Languages[1] && "Échale un vistazo "}
+            {lang === Languages[2] && "Fes-li una ullada "}
+
+            <BsSearch />
+          </p>
+        </a>
       </section>
     </>
   );
