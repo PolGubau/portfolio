f95c623cb809e92d574a7f590ce30e8f8d207c23
Pol Gubau <gubaupol@gmail.com>
2022-09-10 10:33:59 +0200

    Update Card.js

--- src/components/Card/Card/Card.js
+++ src/components/Card/Card/Card.js
@@ -1,7 +1,6 @@
 import React, { memo, useRef } from "react";
 import { motion, useMotionValue } from "framer-motion";
 import { Link } from "react-router-dom";
-import { useInvertedBorderRadius } from "src/components/Card/utils/use-inverted-border-radius";
 import { Title } from "src/components/Card/Title/Title";
 import { Image } from "src/components/Card/Image/Image";
 import Description from "src/components/Card/Description/Description";
@@ -9,6 +8,8 @@ import { openSpring, closeSpring } from "src/components/Card/utils/animations";
 import { useScrollConstraints } from "src/components/Card/utils/use-scroll-constraints";
 import { useWheelScroll } from "src/components/Card/utils/use-wheel-scroll";
 import "./Card.css";
+import { useHistory } from "react-router-dom";
+
 import ChangeProjectButtons from "src/components/Card/ChangeProjectButtons";
 // Distance in pixels a user has to scroll a card down before we recognise
 // a swipe-to dismiss action.
@@ -18,10 +19,9 @@ export const Card = memo(
   ({
     index,
     isSelected,
-    id,
+    id = "",
     title,
     category,
-    history,
     pointOfInterestX,
     pointOfInterestY,
     textColor,
@@ -39,8 +39,7 @@ export const Card = memo(
   }) => {
     const y = useMotionValue(0);
     const zIndex = useMotionValue(isSelected ? 2 : 0);
-
-    const inverted = useInvertedBorderRadius(20);
+    let history = useHistory();
 
     const cardRef = useRef(null);
     const constraints = useScrollConstraints(cardRef, isSelected);
@@ -95,15 +94,13 @@ export const Card = memo(
 
           {/* Card */}
           <Link
-            className={`card-content-container ${
-              isSelected && "open"
-            } card-open-link`}
-            to={id}
+            to={`/${id}`}
+            className={`cardContainer ${isSelected && "open"}`}
           >
             <motion.div
               ref={cardRef}
               className="card-content"
-              style={{ ...inverted, zIndex, y }}
+              style={{ zIndex, y }}
               layoutTransition={
                 isSelected ? !mobile && openSpring : closeSpring
               }
@@ -133,9 +130,9 @@ export const Card = memo(
                 backgroundColor={backgroundColor}
               />
 
-              <motion.div
+              <div
                 className="content-container"
-                style={{ ...inverted, originY: 0, originX: 0 }}
+                style={{ originY: 0, originX: 0 }}
               >
                 <Description
                   tags={tags}
@@ -144,11 +141,9 @@ export const Card = memo(
                   link={link}
                   madeFor={madeFor}
                 />
-              </motion.div>
+              </div>
             </motion.div>
           </Link>
-          {/* Outside Card */}
-          {!isSelected && <Link />}
         </motion.li>
       </>
     );
