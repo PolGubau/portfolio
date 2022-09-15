d9b4d13a39b5c7a99f0ce293e7f4386603f37f5a
Pol Gubau <gubaupol@gmail.com>
2022-09-15 22:16:35 +0200

    Create CardContent.tsx

---
+++ src/components/Card/Card/CardContent.tsx
@@ -0,0 +1,36 @@
+import { memo } from "react";
+import { Title } from "src/components/Card/Title/Title";
+import { Image } from "src/components/Card/Image/Image";
+import Description from "src/components/Card/Description/Description";
+import { CartInterface } from "src/Interfaces";
+
+import "./Card.css";
+import "./Overlay.css";
+
+import ChangeProjectButtons from "src/components/Card/ChangeProjectButtons";
+
+export const CardContent = memo(
+  ({ project, ids, lang, isSelected, mobile }: CartInterface) => {
+    return (
+      <>
+        <ChangeProjectButtons
+          id={project.id}
+          ids={ids}
+          isSelected={isSelected}
+        />
+        <Title lang={lang} project={project} isSelected={isSelected} />
+
+        <Image isSelected={isSelected} project={project} mobile={mobile} />
+
+        <div
+          className={`${
+            mobile && isSelected ? "contentContainerMobile" : "contentContainer"
+          } `}
+          // style={{ originY: 0, originX: 0 }}
+        >
+          <Description lang={lang} project={project} />
+        </div>
+      </>
+    );
+  }
+);
