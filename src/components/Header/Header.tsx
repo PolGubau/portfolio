67d39f28e8b492ce67024b3d534cf39c5d196049
Pol Gubau <gubaupol@gmail.com>
2022-09-16 10:14:49 +0200

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,21 @@ import "./Header.css";
 import LangContext from "src/context/LangContext";
 import { headerText } from "src/Consts";
 import LanguageButtons from "./languagesButtons/LanguageButtons";
+import { HeaderTextInterface } from "src/Interfaces";
 
 export default function Header() {
   const { lang } = useContext(LangContext);
 
+  let text: HeaderTextInterface = headerText.English;
+  if (lang === "Spanish") {
+    text = headerText.Spanish;
+  }
+  if (lang === "English") {
+    text = headerText.English;
+  }
+  if (lang === "Catalan") {
+    text = headerText.Catalan;
+  }
   return (
     <>
       <header>
@@ -18,8 +29,8 @@ export default function Header() {
               <LanguageButtons />
             </div>
 
-            <h1>{headerText[lang].title}</h1>
-            <h3>{headerText[lang].description}</h3>
+            <h1>{text.title}</h1>
+            <h3>{text.description}</h3>
           </article>
 
           <div className="avatar">
