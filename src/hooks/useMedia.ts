3aed4d53ba57be511f19aed04fc4a892d6df01bf
Pol Gubau <gubaupol@gmail.com>
2022-09-17 21:00:36 +0200

    Update useMedia.ts

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 
-const useMedia = () => {
+const useMedia = (width: undefined | number) => {
+  if (!width) width = 700;
   const [isDesktop, setIsDesktop] = useState(false);
   useEffect(() => {
-    const media = window.matchMedia("(min-width: 700px)");
+    const media = window.matchMedia(`(min-width: ${width}px)`);
     const listener = () => setIsDesktop(media.matches);
     listener();
     window.addEventListener("resize", listener);
