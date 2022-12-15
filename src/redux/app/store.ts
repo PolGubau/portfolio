6424d674c21593fcc877f801e9b038a90467d3e6
Pol Gubau <pol.gubau@meona.de>
2022-12-15 16:16:25 +0100

    Pol; Master; redux

--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,20 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import stickiesReducer from "../features/language";
-import categoryReducer from "../features/categoriesSlice";
-import selectedCategoriesReducer from "../features/selectedCategoriesSlice";
-import popupReducer from "../features/popupSlice";
-import settingsReducer from "../features/settingsSlice";
+import languageReducer from "../features/languageSlice";
 export const store = configureStore({
   reducer: {
-    stickies: stickiesReducer,
-    categories: categoryReducer,
-    selectedCategories: selectedCategoriesReducer,
-    popup: popupReducer,
-    settings: settingsReducer,
+    language: languageReducer,
+    
+    
   },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
