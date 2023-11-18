ca887470c5a4724dc4847122d9e7da36b5aa9308
Pol Gubau <gubaupol@gmail.com>
2023-11-18 12:50:11 +0100

    Remove website link from Footer component

--- app/components/Layout/Footer.tsx
+++ app/components/Layout/Footer.tsx
@@ -60,24 +60,6 @@ const Footer = () => {
             <circle cx="4" cy="4" r="2"></circle>
           </svg>
         </Link>
-        <Link href={metadata.links.website} title="Visit my website">
-          <span className="sr-only">Visit my website</span>
-          <svg
-            xlinkTitle="Visit my website"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M15 7h3a5 5 0 0 1 5 5 5 5 0 0 1-5 5h-3m-6 0H6a5 5 0 0 1-5-5 5 5 0 0 1 5-5h3"></path>
-            <line x1="8" y1="12" x2="16" y2="12"></line>
-          </svg>
-        </Link>
       </div>
     </div>
   );
