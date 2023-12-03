4f947ebae2bdd8d8fcdf1c703955762588c7e57a
Pol Gubau <gubaupol@gmail.com>
2023-12-03 12:07:14 +0100

    Remove unsafe script sources in ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -22,8 +22,6 @@ const nextConfig = {
 
 // https://nextjs.org/docs/advanced-features/security-headers
 const ContentSecurityPolicy = `
-    default-src 'self' vercel.live;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' vercel.live va.vercel-scripts.com;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'none';
