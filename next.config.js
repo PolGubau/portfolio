c6be8b4bcc1d1897a732ec4f48a1520589343da6
Pol Gubau <gubaupol@gmail.com>
2023-12-03 10:49:34 +0100

    Update Content Security Policy in next.config.js

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
 // https://nextjs.org/docs/advanced-features/security-headers
 const ContentSecurityPolicy = `
     default-src 'self' vercel.live;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live va.vercel-scripts.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' vercel.live va.vercel-scripts.com;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'none';
