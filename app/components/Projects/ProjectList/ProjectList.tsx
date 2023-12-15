a2ab94a456a15255575023a7e14e002b11d13101
Pol Gubau <gubaupol@gmail.com>
2023-12-15 11:36:36 +0100

    Add react-icons package and update styles in various components

--- app/components/Projects/ProjectList/ProjectList.tsx
+++ app/components/Projects/ProjectList/ProjectList.tsx
@@ -14,17 +14,17 @@ const container = {
 };
 
 const item = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
+  hidden: { opacity: 0, innerWidth: 0 },
+  show: { opacity: 1, innerWidth: "100%" },
 };
 export const ProjectsList = ({ projects }: { projects: Projects[] }) => {
   return (
-    <motion.nav variants={container} initial="hidden" animate="show" className="flex flex-wrap gap-4">
-      {projects.map((p) => (
-        <motion.div variants={item} key={p.slug} className="postGrid ">
-          <ProjectPreview backgroundColor={p.color} title={p.title} slug={p.slug} image={{ src: `/images/${p.slug}/${p.cover}`, alt: "" }} />
-        </motion.div>
-      ))}
-    </motion.nav>
+    <motion.ul variants={ container } initial="hidden" animate="show" className="flex flex-wrap gap-4">
+      { projects.map((p) => (
+        <motion.li variants={ item } key={ p.slug } className="postGrid ">
+          <ProjectPreview backgroundColor={ p.color } title={ p.title } slug={ p.slug } image={ { src: `/images/${p.slug}/${p.cover}`, alt: "" } } />
+        </motion.li>
+      )) }
+    </motion.ul>
   );
 };
