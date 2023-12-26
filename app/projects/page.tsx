68d6de70a76849f1c9812bf77fc72e8d22dbf16f
Pol Gubau <gubaupol@gmail.com>
2023-12-26 17:37:56 +0100

    Update metadata for projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,31 @@
 import type { Metadata } from "next";
 import { allProjects } from "contentlayer/generated";
 import { ProjectsList } from "app/components/Projects/ProjectList/ProjectList";
-export const metadata: Metadata = {
-  title: "Blog",
-  description: "Read my thoughts on software development, design, and more.",
-};
 
+export const metadata: Metadata = {
+  title: "Projects",
+  description: "Discover my recent projects and achievements.",
+  keywords: ["projects", "portfolio", "web development", "coding"],
+  authors: {
+    url: "https://polgubau.com",
+    name: "Pol Gubau Amores",
+  },
+  openGraph: {
+    title: "Projects | Pol Gubau Amores",
+    description: "Discover my recent projects and achievements.",
+    url: "https://polgubau.com/projects",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://polgubau.com/brand/thumbnail.png",
+        width: 1920,
+        height: 1080,
+        alt: "Pol Gubau Amores' website preview",
+      },
+    ],
+    type: "website",
+  },
+};
 const orderedProjects = allProjects.sort((a, b) => {
   return new Date(b.startedAt).getTime() - new Date(a.startedAt).getTime();
 });
