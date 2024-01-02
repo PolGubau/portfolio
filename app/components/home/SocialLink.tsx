849d23c4dd66fbf1e642d5a50fbb3449945e3eea
Pol Gubau <gubaupol@gmail.com>
2024-01-02 10:26:10 +0100

    Refactor header component and update metadata

---
+++ app/components/home/SocialLink.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+interface SocialLinkProps {
+  href: string;
+  label: React.ReactNode;
+  icon: React.ReactNode;
+}
+
+const SocialLink = ({ href, label, icon }: SocialLinkProps) => {
+  return (
+    <Link
+      href={href}
+      className="bg-dark/10 dark:bg-light/10  rounded-full flex gap-2 w-fit py-1 px-3 items-center transition-all hover:shadow-md hover:bg-dark/20 dark:hover:bg-light/20"
+    >
+      {icon}
+      {label}
+    </Link>
+  );
+};
+
+export default SocialLink;
